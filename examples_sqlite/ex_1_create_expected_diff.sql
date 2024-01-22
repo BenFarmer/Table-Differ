fdf779da43a19d6b23f13f80892e26e92163826a
Benjamin Farmer <benjaminrfarmer@gmail.com>
2024-01-22 15:47:54 -0700

    add: sqlite examples for testing

---
+++ examples_sqlite/ex_1_create_expected_diff.sql
@@ -0,0 +1,19 @@
+CREATE TABLE ex_1_expected_diff (
+    initial_cust_id                     INT         ,
+    secondary_cust_id                   INT         ,
+    initial_cust_name                   VARCHAR     ,
+    secondary_cust_name                 VARCHAR     ,
+    initial_status                      VARCHAR     ,
+    secondary_status                    VARCHAR     ,
+    initial_contract_start_date         DATE        ,
+    secondary_contract_start_date       DATE        ,
+    initial_contract_end_date           DATE        ,
+    secondary_contract_end_date         DATE        ,
+    initial_contract_credits            INT         ,
+    secondary_contract_credits          INT         ,
+    initial_contract_balance            INT         ,
+    secondary_contract_balance          INT         ,
+    initial_latest_billing_period       DATE        ,
+    secondary_latest_billing_period     DATE        
+    )
+;
---
+++ examples_sqlite/ex_1_create_secondary.sql
@@ -0,0 +1,11 @@
+CREATE TABLE ex_1_secondary (
+    cust_id                 INT         NOT NULL,
+    cust_name               VARCHAR     NOT NULL,
+    status                  VARCHAR     NOT NULL,
+    contract_start_date     DATE        NOT NULL,
+    contract_end_date       DATE                , 
+    contract_credits        INT         NOT NULL,
+    contract_balance        INT         NOT NULL,
+    latest_billing_period   Date        NOT NULL
+    )
+;
